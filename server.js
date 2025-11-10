80d7b26d7058db2a0209d09a7852c6c7a1a979a1
finnsueess3-sudo <finn.sueess3@gmail.com>
2025-11-10 09:57:00 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,40 +1,24 @@
-// server.js
-const express = require("express");
-const http = require("http");
-const { Server } = require("socket.io");
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-app.use(express.static("public"));
+io.on('connection', (socket) => {
+  console.log('Client connected:', socket.id);
 
-let players = {};
-
-io.on("connection", (socket) => {
-  console.log("Neuer Spieler:", socket.id);
-  players[socket.id] = { x:0, y:1, z:0, hp:100 };
-
-  socket.emit("currentPlayers", players);
-  socket.broadcast.emit("newPlayer", { id: socket.id, ...players[socket.id] });
-
-  socket.on("move", (data) => {
-    players[socket.id] = data;
-    socket.broadcast.emit("playerMoved", { id: socket.id, ...data });
+  socket.on('playerMove', (data) => {
+    // Broadcast move to other clients
+    socket.broadcast.emit('playerMove', data);
   });
 
-  socket.on("attack", targetId => {
-    if(players[targetId]){
-      players[targetId].hp -= 10;
-      io.emit("playerHit", { id: targetId, hp: players[targetId].hp });
-    }
-  });
-
-  socket.on("disconnect", () => {
-    delete players[socket.id];
-    io.emit("playerDisconnected", socket.id);
+  socket.on('disconnect', () => {
+    console.log('Client disconnected:', socket.id);
   });
 });
 
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log("Server läuft auf Port", PORT));
+server.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
